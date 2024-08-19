3a9bc68f742732bd4d0d9c4635d51e9783993fc7
Hira Amanat <134780461+iAmHira19@users.noreply.github.com>
2024-08-19 15:29:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("tripForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    // Here you can add your form validation logic and submit data
+    console.log("Form submitted!");
+});
